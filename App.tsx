45a44d85a271439eb21749d4dc251bb6a9ffc07e
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-26 13:41:24 +0900

    firebaseを初期化

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import firebase from 'firebase/app';
+import firebaseConfig from './firebase-config';
 import MainTabNavigator from './src/navigations/MainTabNavigator';
+
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
 	return (
