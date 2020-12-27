ca667c60f9ff8899e0781a45b15db0d321851758
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-27 12:42:51 +0900

    Ownerモデルにタイムスタンプを追加

--- src/services/models/owner.ts
+++ src/services/models/owner.ts
@@ -1,7 +1,11 @@
+import firebase from 'firebase/app';
+
 export type Owner = {
   id?: string;
   familyName: string;
   givenName: string;
   name: string;
   photoUrl: string;
+  createdAt: firebase.firestore.Timestamp | null;
+  updatedAt: firebase.firestore.Timestamp | null;
 };
