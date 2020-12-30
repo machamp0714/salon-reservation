ce84850718e2ae58db6193daa529d8186fe8e252
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-30 21:06:08 +0900

    fix

--- src/services/models/reservation.ts
+++ src/services/models/reservation.ts
@@ -5,7 +5,8 @@ export type Reservation = {
   startDate: firebase.firestore.Timestamp;
   endDate: firebase.firestore.Timestamp;
   color: string;
-  customerName: string;
+  customerFirstName: string;
+  customerLastName: string;
   ownerId: string;
   createdAt: firebase.firestore.Timestamp | null;
   updatedAt: firebase.firestore.Timestamp | null;
