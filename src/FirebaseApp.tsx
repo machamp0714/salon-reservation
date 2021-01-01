e698c5ce67703683baa0ff4fdf12211768b6e91c
machamp0714 <rateliasudaisuki@gmail.com>
2021-01-01 14:47:05 +0900

    console.logに警告を出す

--- src/FirebaseApp.tsx
+++ src/FirebaseApp.tsx
@@ -25,7 +25,7 @@ const FirebaseApp: FC = ({ children }) => {
 
   useEffect(() => {
     return unsubscribed;
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [unsubscribed]);
 
   return (
     <FirebaseContext.Provider value={{ auth, db }}>
