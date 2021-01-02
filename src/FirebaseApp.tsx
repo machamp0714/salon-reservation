12d2f014be2bc9e89c2c6da8971bf8677833d3ee
machamp0714 <rateliasudaisuki@gmail.com>
2021-01-02 15:52:16 +0900

    RootScreenを作成

--- src/FirebaseApp.tsx
+++ src/FirebaseApp.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -6,26 +6,12 @@ import 'firebase/firestore';
 import { FirebaseContext, OwnerContext } from './contexts';
 
 import { Owner } from './services/models/owner';
-import findOwner from './services/find-owner';
 
 const FirebaseApp: FC = ({ children }) => {
   const [owner, setOwner] = useState<Owner | null>(null);
 
   const auth = firebase.auth();
   const db = firebase.firestore();
-
-  useEffect(() => {
-    const unsubscribed = auth.onAuthStateChanged(async (firebaseUser) => {
-      if (firebaseUser) {
-        const owner = await findOwner(db, firebaseUser.uid);
-        setOwner(owner);
-      } else {
-        setOwner(null);
-      }
-    });
-
-    return unsubscribed;
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <FirebaseContext.Provider value={{ auth, db }}>
