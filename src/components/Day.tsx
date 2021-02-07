08ba49e2772c7b2bc3ab480ec358bccb67a4de26
machamp0714 <rateliasudaisuki@gmail.com>
2021-02-07 14:52:06 +0900

    add: searchOptions to use-reservations

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import useReservations from '../hooks/use-reservations';
 
 interface DayComponentProps {
   date: {
@@ -9,11 +10,16 @@ interface DayComponentProps {
     timestamp: number;
     year: number;
   };
-  testID: string;
   accessibilityLable: string;
 }
 
 const Day: FC<DayComponentProps> = ({ date, accessibilityLable, children }) => {
+  const { reservations } = useReservations({
+    year: date.year,
+    month: date.month,
+    day: date.day,
+  });
+
   return (
     <View accessibilityLabel={accessibilityLable} style={styles.day}>
       <Text style={styles.text}>{children}</Text>
